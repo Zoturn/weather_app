c8e53616abe696a18f418426e04c63ffe21bab81
Zoturn <satturrn@gmail.com>
2017-11-17 15:04:53 +0200

    some stiles

--- src/App.js
+++ src/App.js
@@ -7,10 +7,13 @@ import {inject, observer} from 'mobx-react';
 class App extends Component {
   hendleSubmit = (e) => {
     e.preventDefault();
-    const city = this.city.value;
+    let city = this.city.value;
     this.props.WeatherStore.AddCity(city);
     this.city.value = '';
+  }
 
+  capitalize = (s) => {
+    return s && s[0].toUpperCase() + s.slice(1);
   }
 
   componentDidMount() {
@@ -31,36 +34,41 @@ class App extends Component {
         <h2>Weather in the City. You have {WeatherStore.weatherCount}</h2>
 
         <form onSubmit={e => this.hendleSubmit(e)}>
-          <input type="text" placeholder="Enter city" ref={input => this.city = input}/>
+          <input id="cityInput" type="text" placeholder="Enter city" ref={input => this.city = input}/>
           <button>Add City</button>
         </form>
 
         <table>
           <tbody>
+          <tr>
+            <th>city</th>
+            <th>weather</th>
+          </tr>
           {WeatherStore.currentCity.map(city => (
-            <tr key={city.city}>
+            <tr id={city.city} key={city.city}>
               <td>
-                <div id={city}>
-                  {city.city + ' - ' + city.weather}
-                  <input className={city.city} type={'submit'} value={'delete'} onClick={this.deleteCity}/>
-                </div>
+                {city.city}
               </td>
+              <td>
+                {city.weather}
+              </td>
+              <td><input className={city.city} type={'submit'} value={'delete'} onClick={this.deleteCity}/></td>
             </tr>
           ))}
           {localStorage.setItem("city", JSON.stringify(WeatherStore.city))}
           {WeatherStore.city.map(city => (
-            <tr key={city.city}>
+            <tr id={city.city} key={city.city}>
               <td>
-                <div id={city}>
-                  {city.city + ' - ' + city.weather}
-                  <input className={city.city} type={'submit'} value={'delete'} onClick={this.deleteCity}/>
-                </div>
+                {city.city}
               </td>
+              <td>
+                {city.weather}
+              </td>
+              < td>< input className={city.city} type={'submit'} value={'delete'} onClick={this.deleteCity}/></td>
             </tr>
           ))}
           </tbody>
         </table>
-
       </div>
     );
   }
--- src/stores/WeatherStore.js
+++ src/stores/WeatherStore.js
@@ -1,22 +1,32 @@
 import {observable, action, computed} from 'mobx';
 
 let storageCity
-if (localStorage.getItem("city") === null){
+if (localStorage.getItem("city") === null) {
   storageCity = []
-}else {storageCity = JSON.parse(localStorage.getItem("city"))}
+} else {
+  storageCity = JSON.parse(localStorage.getItem("city"))
+}
 
 class WeatherStore {
   @observable city = storageCity;
   @observable currentCity = [];
 
   @action AddCity = (city) => {
+
     let cityAndWeather = {}
-    fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=a96ef5cb83edc2065f75c5e6e13c79e8')
+
+    function capitalize(city) {
+      return city.charAt(0).toUpperCase() + city.slice(1);
+    }
+
+    city = capitalize(city)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=a96ef5cb83edc2065f75c5e6e13c79e8`)
       .then(response => {
         return response.json()
       })
       .then(result => {
-        if (city !== "") {
+        const filteredCity = (this.city).filter(el => el.city === city ? false : true)
+        if (city !== "" && result.main !== undefined && filteredCity.length === this.city.length) {
           let weather = result.main
           let now
           for (let key in weather) {
@@ -25,6 +35,10 @@ class WeatherStore {
           }
           cityAndWeather.city = city
           this.city.push(cityAndWeather)
+        } else if (filteredCity.length !== this.city.length) {
+          alert('This city already added')
+        } else {
+          alert('There is no such city')
         }
       })
       .catch(error => console.log(error))
@@ -35,20 +49,19 @@ class WeatherStore {
     this.currentCity = (this.currentCity).filter(el => el.city === Name ? false : true)
   }
 
-  @action current() {
-    let lat
-    let lon
-    navigator.geolocation.getCurrentPosition(async function (position) {
-      lat = position.coords.latitude;
-      lon = position.coords.longitude;
-      await getWeather(lat, lon)
+  @action
+  current() {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      getWeather(lat, lon)
     });
 
     const thisCity = this.currentCity
 
     function getWeather(lat, lon) {
       let cityAndWeather = {}
-      fetch('http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&APPID=a96ef5cb83edc2065f75c5e6e13c79e8')
+      fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=a96ef5cb83edc2065f75c5e6e13c79e8`)
         .then(response => response.json())
         .then(result => {
           let weather = result.main
@@ -70,9 +83,6 @@ class WeatherStore {
   get weatherCount() {
     return this.city.length;
   }
-
-
-
 }
 
 const store = new WeatherStore();
